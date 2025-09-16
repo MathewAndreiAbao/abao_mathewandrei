31609413868a0f75060c875bf3cb0e71c8164881
MathewAndreiAbao <abaomathew91625@gmail.com>
2025-09-16 14:30:54 +0800

    Update routes.php

--- app/config/routes.php
+++ app/config/routes.php
@@ -2,20 +2,17 @@
 defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 // Auth routes
-$router->get('/', 'Loan_Controller::read');
 $router->get('/login', 'Auth_Controller::login');
 $router->post('/login', 'Auth_Controller::do_login');
 $router->get('/register', 'Auth_Controller::register');
 $router->post('/register', 'Auth_Controller::do_register');
 $router->post('/logout', 'Auth_Controller::logout');
 
-// Profile routes (protected in controller)
-$router->get('/profile', 'Profile_Controller::index');
-$router->post('/profile/update', 'Profile_Controller::update');
+// Profile routes (for avatar upload/modify)
 $router->post('/profile/avatar', 'Profile_Controller::upload_avatar');
 
-// Loan routes
-$router->get('/loans', 'Loan_Controller::read');
+// Loan/Index routes (protected in controller)
+$router->get('/', 'Loan_Controller::read');
 $router->post('/create-loan', 'Loan_Controller::createLoan');
 $router->post('/update-loan/{id}', 'Loan_Controller::updateLoan');
 $router->post('/delete-loan/{id}', 'Loan_Controller::deleteLoan');
