6b0623e6bbefa8a6b41f4d7cd026bb47f696cd67
MathewAndreiAbao <abaomathew91625@gmail.com>
2025-09-16 15:10:44 +0800

    Update routes.php

--- app/config/routes.php
+++ app/config/routes.php
@@ -8,11 +8,13 @@ $router->get('/register', 'Auth_Controller::register');
 $router->post('/register', 'Auth_Controller::do_register');
 $router->post('/logout', 'Auth_Controller::logout');
 
-// Profile routes (for avatar upload/modify)
+// Profile routes (protected in controller)
+$router->get('/profile', 'Profile_Controller::index');
+$router->post('/profile/update', 'Profile_Controller::update');
 $router->post('/profile/avatar', 'Profile_Controller::upload_avatar');
 
-// Loan/Index routes (protected in controller)
-$router->get('/', 'Loan_Controller::read');
+// Loan routes (protected in controller)
+$router->get('/', 'Loan_Controller::read'); // Serves index.php
 $router->post('/create-loan', 'Loan_Controller::createLoan');
 $router->post('/update-loan/{id}', 'Loan_Controller::updateLoan');
 $router->post('/delete-loan/{id}', 'Loan_Controller::deleteLoan');
