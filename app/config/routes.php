77e430f0154de20b7882025a04c7c60e33f502b7
MathewAndreiAbao <abaomathew91625@gmail.com>
2025-09-19 08:44:03 +0800

    Update routes.php

--- app/config/routes.php
+++ app/config/routes.php
@@ -1,8 +1,60 @@
 <?php
 defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
+/**
+ * ------------------------------------------------------------------
+ * LavaLust - an opensource lightweight PHP MVC Framework
+ * ------------------------------------------------------------------
+ *
+ * MIT License
+ *
+ * Copyright (c) 2020 Ronald M. Marasigan
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @package LavaLust
+ * @author Ronald M. Marasigan <ronald.marasigan@yahoo.com>
+ * @since Version 1
+ * @link https://github.com/ronmarasigan/LavaLust
+ * @license https://opensource.org/licenses/MIT MIT License
+ */
 
-$router->get('/', 'Loan_Controller::read');
-$router->post('/create-loan', 'Loan_Controller::createLoan');
-$router->post('/update-loan/{id}', 'Loan_Controller::updateLoan');
-$router->post('/delete-loan/{id}', 'Loan_Controller::deleteLoan');
-?>
+/*
+| -------------------------------------------------------------------
+| URI ROUTING
+| -------------------------------------------------------------------
+| Here is where you can register web routes for your application.
+|
+|
+*/
+
+$router->match(['get','post'], '/', 'AuthController::login');
+
+// Auth
+$router->match(['get','post'], '/auth/signup', 'AuthController::signup');
+$router->match(['get','post'], '/auth/login', 'AuthController::login');
+$router->get('/auth/logout', 'AuthController::logout');
+
+// Tasks
+$router->get('/tasks', 'TaskController::index');
+$router->match(['get','post'], '/tasks/create', 'TaskController::create');
+$router->get('/tasks/view/{id}', 'TaskController::view')->where_number('id');
+$router->get('/tasks/view', 'TaskController::view');
+$router->match(['get','post'], '/tasks/edit/{id}', 'TaskController::edit')->where_number('id');
+$router->match(['get','post'], '/tasks/edit', 'TaskController::edit');
+$router->post('/tasks/delete/{id}', 'TaskController::delete')->where_number('id');
