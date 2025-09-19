aa0b9b08d3a0024161236e6769c1ed928487c554
MathewAndreiAbao <abaomathew91625@gmail.com>
2025-09-19 09:37:37 +0800

    Create deleted.php

---
+++ app/views/tasks/deleted.php
@@ -0,0 +1,11 @@
+<?php
+ob_start();
+?>
+<?php
+// Deleted items feature has been removed from this application.
+// This view is retained as a placeholder in case the feature is reintroduced later.
+?>
+<?php
+$content = ob_get_clean();
+require APP_DIR . 'views/layout.php';
+?>
